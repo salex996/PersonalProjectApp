e7b5511155faa8282f6d48a714654d26e2fc62f7
salex996 <alejandromsm96@gmail.com>
2024-01-27 17:16:50 +0100

    first commit

--- lib/custom_code/actions/find_devices.dart
+++ lib/custom_code/actions/find_devices.dart
@@ -20,36 +20,41 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 Future<List<IBeaconModel>>findDevices() async {
 
 
+  StreamSubscription<List<ScanResult>?>? _scanSubscription;
   Map<String, dynamic> devicesMap = {};
   List<IBeaconModel> beacons = [];
   bool scanning = false;
 
-  List<ScanResult> scanResult;
- late StreamSubscription<List<ScanResult>?>? _scanSubscription;
+
+int i = 0;
+int p = 0;
+int t = 0;
+  List<ScanResult> scanResult = [];
+
   //Start Scanning
-  await FlutterBluePlus.startScan(timeout: const Duration(seconds: 4), androidUsesFineLocation: true);
+  await FlutterBluePlus.startScan(timeout: const Duration(seconds: 10), androidUsesFineLocation: true);
+  scanning = true;
 
-
-  _scanSubscription = FlutterBluePlus.scanResults.listen((scanResult) {
-
-    for (ScanResult result in scanResult) {
+   _scanSubscription = FlutterBluePlus.scanResults.listen((scanResult) {
+  //var _scanSubscription = FlutterBluePlus.scanResults.listen((scanResult) {
+    for (var result in scanResult) {
+      i=i+1;
       Map<String, dynamic> iBeaconData = convertAdvertisementDataToIBeaconData(result);
-
       if (iBeaconData.isNotEmpty) {
+        p=p+1;
         IBeaconModel beacon = IBeaconModel.fromMap(iBeaconData);
-
         // Add to list of beacons if not already in list based on remoteID
         if (!beacons.any((element) => element.remoteId == beacon.remoteId)) {
-
+            t=t+1;
             beacons.add(beacon);
 
         }
       }
     }
   });
-
   FlutterBluePlus.cancelWhenScanComplete(_scanSubscription);
   await FlutterBluePlus.isScanning.where((value) => value == false).first;
+  scanning = false;
 
   return beacons;
 
@@ -58,27 +63,27 @@ Future<List<IBeaconModel>>findDevices() async {
 
 
 
-Map<String, dynamic> convertAdvertisementDataToIBeaconData(ScanResult results) {
+Map<String, dynamic> convertAdvertisementDataToIBeaconData(ScanResult scanResult) {
   Map<String, dynamic> iBeaconData = {};
 
-  if (results.advertisementData.manufacturerData.containsKey(76) &&
-      results.advertisementData.manufacturerData[76]!.length == 23 &&
-      results.advertisementData.manufacturerData[76]![0] == 2 &&
-      results.advertisementData.manufacturerData[76]![1] == 21) {
+  if (scanResult.advertisementData.manufacturerData.containsKey(76) &&
+      scanResult.advertisementData.manufacturerData[76]!.length == 23 &&
+      scanResult.advertisementData.manufacturerData[76]![0] == 2 &&
+      scanResult.advertisementData.manufacturerData[76]![1] == 21) {
     // print(">>>>> iBeacon found: ${scanResult.advertisementData.manufacturerData[76]}");
-    List<int>? data = results.advertisementData.manufacturerData[76];
-    iBeaconData['remoteId'] = results.device.remoteId.str;
-    iBeaconData['platformName'] = results.device.platformName;
+    List<int>? data = scanResult.advertisementData.manufacturerData[76];
+    iBeaconData['remoteId'] = scanResult.device.remoteId.str;
     iBeaconData['type'] = data![0];
     iBeaconData['length'] = data[1];
     iBeaconData['uuid'] = IBeaconModel.convertToUUID(data.sublist(2, 18));
     iBeaconData['major'] = IBeaconModel.convertToInt(data.sublist(18, 20));
     iBeaconData['minor'] = IBeaconModel.convertToInt(data.sublist(20, 22));
     iBeaconData['pwr'] = data[22];
-    iBeaconData['rssi'] = results.rssi;
+    iBeaconData['rssi'] = scanResult.rssi;
   }
 
   return iBeaconData;
 }
+
 
 
--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -1,4 +1,4 @@
 export 'find_devices.dart' show findDevices;
 export 'is_bluetooth_enabled.dart' show isBluetoothEnabled;
 export 'turn_on_bluetooth.dart' show turnOnBluetooth;
-export 'convertadvtodata.dart' show convertadvdata;
+
